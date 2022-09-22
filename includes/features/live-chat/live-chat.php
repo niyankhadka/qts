6206fbcb0308a67cdd0ea17833882db54687373a
niyankhadka <niyankhadka.nk@gmail.com>
2022-09-23 00:32:35 +0545

    live chat file edited

---
+++ includes/features/live-chat/live-chat.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Live chat features function of the plugin
+ *
+ * @since      1.0.0
+ * @package    QTS
+ * @subpackage QTS/includes/features/live-chat
+ * @author     niyankhadka <niyankhadka.nk@gmail.com>
+ */
+
+// Exit if accessed directly.
+if ( !defined( 'ABSPATH' ) ) {
+
+    exit;
+}
